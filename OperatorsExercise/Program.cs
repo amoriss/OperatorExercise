710539ba7812f2cfcf6b591e8e4b941e7f7a20ee
Amoriss <amoriss@protonmail.com>
2021-08-20 15:11:52 -0500

    initial commit

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder a % b;
+
+            
+            Console.WriteLine($" {a} / {b} is {quotient} remainder {remainder}");
+            
+                
+            
+        }
+    }
+}
